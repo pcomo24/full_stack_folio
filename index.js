32a4b81dd19167332e4cf604e513c96d2aa98aab
pcomo24 <pcomo24@gmail.com>
2017-07-30 22:32:58 -0500

    ready for deploy

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const aws = require('aws-sdk');
-const menuspy = require('menuspy');
 
 //set static folder
 app.use('/public', express.static('public'));
-app.use('/smooth', express.static('node_modules/smooth-scroll/dist/js'));
-app.use('/anime', express.static('node_modules/animejs'));
-app.use('/menuspy', express.static('node_modules/menuspy/dist'));
 
 //use handlebars
 app.set('view engine', 'hbs');
+
 //body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -22,12 +19,13 @@ aws.config.loadFromPath('config.json');
 const ses = new aws.SES({apiVersion: '2010-12-01'})
 const to = ['pcomo24@gmail.com']
 const from = 'pcomo24@gmail.com';
-//urls and views
 
+//Routing
 app.get('/', function (req, res) {
     res.render('home.hbs')
 });
 
+//post from form on home.hbs
 app.post('/sendMsg', function (req, res) {
     let msgName = req.body.name;
     let msgPhone = req.body.phone;
@@ -35,13 +33,8 @@ app.post('/sendMsg', function (req, res) {
     let msgMsg = req.body.msg;
     let dataTemp = `<h4>NAME: ${msgName}</h4><h4>EMAIL: ${msgEmail}</h4><h4>PHONE: ${msgPhone}</h4>
                     <h4>MSG: </h4><p>${msgMsg}</p>`;
-    //test message info is being passed back
-    console.log(`NAME: ${msgName}`);
-    console.log(`PHONE: ${msgPhone}`);
-    console.log(`EMAIL: ${msgEmail}`);
-    console.log(`MSG: ${msgMsg}`);
-    //nodemailer info
 
+    //AWS SDK mailer
     ses.sendEmail( {
         Source: from,
         Destination: { ToAddresses: to },
@@ -67,7 +60,7 @@ app.post('/sendMsg', function (req, res) {
     res.redirect('/');
 });
 
-
+//set server location here
 app.listen(8500, function () {
     console.log('your portfolio page is running on 8500')
 });
